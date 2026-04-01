9c1fa31d579b500f5dc2cb46d1b994d7686d4285
HuYunhao <hnu_huyunhao1995@163.com>
2026-04-01 09:23:05 +0800

    修改包的结构

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tlbb_douzi',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Douzi',
+    description='A package for TLBB game automation',
+    python_requires='>=3.11',
+)
+
